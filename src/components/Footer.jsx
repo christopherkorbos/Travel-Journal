9e56056d6b90a5696925e884bd74c26d50aaac4f
unknown <christopher.kormpos@gmail.com>
2022-12-22 23:36:46 +0200

    fixed broken links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,8 +7,8 @@ export default function Footer() {
         <div className="footer">
             <p>Created for the <a href='https://reactjs.org/'>React </a>course by Christopher Korbos ©</p>
             <div>
-                <a href="https://www.linkedin.com/in/christopher-korbos-27808b194/"><img src={Linkedin} alt="linkedin-icon" className='linkedin' /></a>
-                <a href="https://github.com/christopherkorbos/"><img src={Github} alt="github-icon" className='github' /></a>
+                <a href="https://www.linkedin.com/in/christopher-kormpos-27808b194/"><img src={Linkedin} alt="linkedin-icon" className='linkedin' /></a>
+                <a href="https://github.com/christopherkormpos"><img src={Github} alt="github-icon" className='github' /></a>
             </div>
         </div>
     )
